d5ff41a64ce76fd6fbfe30e0a5d90f11b971660e
Jaime Chavarriaga <jaime.chavarriaga@gmail.com>
2024-11-01 03:30:49 +0000

    Agrega caso de uso basico con una prueba

---
+++ tareas/src/main/java/org/swlabs/tareas/tarea/TareaUseCase.java
@@ -0,0 +1,12 @@
+package org.swlabs.tareas.tarea;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TareaUseCase {
+
+    public int suma(int a, int b) {
+        return a + b;
+    }
+
+}
